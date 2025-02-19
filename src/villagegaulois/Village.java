26a3df944ddf261aca5b4793fb0697be7b7cc25a
Bastien Nguyen <ngb5069a@ensfsi.onmicrosoft.com>
2025-02-19 11:48:32 +0100

    fin seance 1

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -21,6 +21,72 @@ public class Village {
 	public void setChef(Chef chef) {
 		this.chef = chef;
 	}
+	
+	private static class Marche{
+		private int nbEtal;
+		private Etal[] tab;
+		private Marche(int nbEtal) {
+			this.nbEtal=nbEtal;
+			tab = new Etal[nbEtal] ;
+			for (int i=0; i<nbEtal;i++) {
+				tab[i]=new Etal();
+			}
+			
+		}
+
+		
+		
+		private void utiliserEtal(int indiceEtal, Gaulois vendeur,
+				String produit, int nbProduit) {
+			tab[indiceEtal].occuperEtal(vendeur,produit,nbProduit);
+		}
+		int trouverEtalLibre() {
+			for (int i=0; i<tab.length;i++) {
+				if (tab[i].isEtalOccupe()==false) {
+					return i;
+				}
+			}
+		return -1;	
+		}
+		
+		private Etal[] trouverEtals(String produit) {
+			int nbetalproduit=0;
+			for (int i=0; i<nbEtal;i++) {
+				if (tab[i].isEtalOccupe()==true && tab[i].contientProduit(produit)==true) {
+					nbetalproduit=nbetalproduit+1;
+				}
+			}
+			
+			Etal[] tab2 = new Etal[nbetalproduit];			
+			int k=0;
+			
+			for (int j=0; j<nbEtal;j++) {
+				if (tab[j].isEtalOccupe()==true && tab[j].contientProduit(produit)==true) {
+					tab2[k]=tab[j];
+					k++;
+				}
+			}
+			return tab2;
+		}
+		
+		private Etal trouverVendeur(Gaulois gaulois) {
+			for (int i=0; i<nbEtal;i++) {
+				if (tab[i].isEtalOccupe()==true && tab[i].getVendeur()==gaulois) {
+					return tab[i];
+				}
+			}
+			return null;
+		}
+		private String afficherMarche() {
+			String retour="\n";
+			for (int i=0; i<nbEtal;i++) {
+				if (tab[i].isEtalOccupe()==true) {
+					retour = retour +"\n" + tab[i].getVendeur() +" vend " ;
+				}
+			}
+			return "";
+		}
+	}
 
 	public void ajouterHabitant(Gaulois gaulois) {
 		if (nbVillageois < villageois.length) {
